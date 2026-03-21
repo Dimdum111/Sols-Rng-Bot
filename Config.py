cde846d341659aaa9ded844c697671966a5961fc
Dimdum <tanomi364364@gmail.com>
2026-03-21 18:23:41 +0300

    v1.0.0 - Refactoring, And new stuff

---
+++ Config.py
@@ -0,0 +1,676 @@
+# Все ауры в игре
+
+auras = {
+
+    "Common": 2,
+
+    "Uncommon": 4,
+
+    "Good": 5,
+
+    "Natural": 8,
+
+    "Rare": 16,
+
+    "Divinus": 32,
+
+    "Crystallised": 64,
+
+    "Rage": 128,
+
+    "Topaz": 150,
+
+    "Ruby": 350,
+
+    "Forbidden": 404,
+
+    "Emerald": 500,
+
+    "Gilded": 512,
+
+    "Ink": 700,
+
+    "Jackpot": 777,
+
+    "Sapphire": 800,
+
+    "Aquamarine": 900,
+
+    "Wind": 900,
+
+    "Diaboli": 1004,
+
+    "Precious": 1024,
+
+    "Atomic": 1180,
+
+    "Glock": 1700,
+
+    "Magnetic": 2048,
+
+    "Ash": 2300,
+
+    "Glacier": 2304,
+
+    "Player": 3000,
+
+    "Flora": 3700,
+
+    "Cola": 3999,
+
+    "Sidereum": 4096,
+
+    "Bleeding": 4444,
+
+    "Flushed": 6900,
+
+    "Hazard": 7000,
+
+    "Quartz": 8192,
+
+    "Honey": 8335,
+
+    "Lost Soul": 9200,
+
+    "Atomic : Riboneucleic": 9876,
+
+    "Undead": 12000,
+
+    "Corrosive": 12000,
+
+    "Rage : Heated": 12800,
+
+    "Ink : LEAK": 14000,
+
+    "Powered": 16384,
+
+    "Copper": 29000,
+
+    "WATT": 32768,
+
+    "Aquatic": 40000,
+
+    "Solar": 50000,
+
+    "Lunar": 50000,
+
+    "Starlight": 50000,
+
+    "Star Rider": 50000,
+
+    "Flushed : Lobotomy": 69000,
+
+    "Hazard : Rays": 70000,
+
+    "Nautilus": 70000,
+
+    "Permafrost": 73500,
+
+    "Flow": 87000,
+
+    "Stormal": 90000,
+
+    "Exotic": 99999,
+
+    "Diaboli : Void": 100400,
+
+    "Comet": 120000,
+
+    "Divinus : Angel": 120000,
+
+    "Jade": 125000,
+
+    "Spectre": 140000,
+
+    "Jazz": 160000,
+
+    "Aether": 180000,
+
+    "Bounded": 200000,
+
+    "Celestial": 350000,
+
+    "Terror": 400000,
+
+    "Warlock": 666000,
+
+    "Undead : Devil": 666666,
+
+    "Kyawthuite": 850000,
+
+    "Arcane": 1000000,
+
+    "Magnetic : Reverse Polarity": 1024000,
+
+    "Undefined": 1111000,
+
+    "Rage : Brawler": 1280000,
+
+    "Astral": 1336000,
+
+    "Cosmos": 1520000,
+
+    "Gravitational": 2000000,
+
+    "Bounded : Unbound": 2000000,
+
+    "Virtual": 2500000,
+
+    "Parasite": 3000000,
+
+    "Savior": 3200000,
+
+    "Shift lock": 3325000,
+
+    "Cosmos : Alice": 3500000,
+
+    "Aquatic : Flame": 4000000,
+
+    "Poseidon": 4000000,
+
+    "Zeus": 4500000,
+
+    "Solar : Solstice": 5000000,
+
+    "Galaxy": 5000000,
+
+    "Lunar : Full Moon": 5000000,
+
+    "Twilight": 6000000,
+
+    "Origin": 6500000,
+
+    "Hades": 6666666,
+
+    "Celestial : Divine": 7000000,
+
+    "Anubis": 7200000,
+
+    "Hyper-Volt": 7500000,
+
+    "Velocity": 7630000,
+
+    "Nautilus : Lost": 7700000,
+
+    "Harnessed": 8500000,
+
+    "Nihility": 9000000,
+
+    "Helios": 9000000,
+
+    "Stargazer": 9200000,
+
+    "Starscourge": 10000000,
+
+    "Sailor": 12000000,
+
+    "Stormal : Hurricane": 13500000,
+
+    "Sirius": 14000000,
+
+    "Arcane : Legacy": 15000000,
+
+    "Lullaby": 17000000,
+
+    "Chromatic": 20000000,
+
+    "Aviator": 24000000,
+
+    "Blizzard": 27315000,
+
+    "Arcane : Dark": 30000000,
+
+    "Ethereal": 35000000,
+
+    "Virtual : Fatal Error": 40413000,
+
+    "Overseer": 45000000,
+
+    "Exotic : Apex": 49999500,
+
+    "Matrix": 50000000,
+
+    "Runic": 50000000,
+
+    "Sentinel": 60000000,
+
+    "Twilight : Iridescent Memory": 60000000,
+
+    "Antivirus": 62500000,
+
+    "Carriage": 80000000,
+
+    "Sailor : Flying Dutchman": 80000000,
+
+    "Harnessed : Elements": 85000000,
+
+    "Virtual : WorldWide": 87500000,
+
+    "Chromatic : Genesis": 99999999,
+
+    "Starscourge : Radiant": 100000000,
+
+    "Overture": 150000000,
+
+    "Symphony": 175000000,
+
+    "Twilight : Withering Grace": 180000000,
+
+    "Felled": 180000000,
+
+    "Impeached": 200000000,
+
+    "Lumenpool": 220000000,
+
+    "Hyper-Volt : Ever-Storm": 225000000,
+
+    "Archangel": 250000000,
+
+    "Astral : Zodiac": 267000000,
+
+    "Exotic : Void": 299999999,
+
+    "Overture : History": 300000000,
+
+    "Bloodlust": 300000000,
+
+    "Maelstrom": 309999999,
+
+    "Lotusfall": 320000000,
+
+    "Jazz : Orchestra": 336870912,
+
+    "Atlas": 360000000,
+
+    "Flora : Evergreen": 370073730,
+
+    "Chillsear": 375000000,
+
+    "Abyssal Hunter": 400000000,
+
+    "Gargantua": 430000000,
+
+    "Apostolos": 444000000,
+
+    "Kyawthuite : Remembrance": 450000000,
+
+    "Ruins": 500000000,
+
+    "Matrix : Overdrive": 503000000,
+
+    "Sophyra": 570000000,
+
+    "Matrix : Reality": 601020102,
+
+    "Sovereign": 750000000,
+
+    "Ruins : Withered": 800000000,
+
+    "Aegis": 825000000,
+
+    "Pixelation": 1073741824,
+
+    "Luminosity": 1200000000,
+
+    "Equinox": 2500000000,
+
+    # Новые ауры для биомов
+
+    "Oppression": 220000000,
+
+    "Glitch": 12210110,
+
+    "Fault": 3000,
+
+    "⭐": 100,
+
+    "⭐⭐": 1000,
+
+    "⭐⭐⭐": 10000,
+
+    "Dreammetric": 520000000,
+
+}
+
+# и лимбо ауры
+
+limbo_auras = {
+
+    "Nothing": 1,
+
+    # Ауры из Null биома (базовый шанс / 1000, так как в Null множитель x1000)
+
+    "Undefined": 1111,
+
+    "Shift lock": 3325,
+
+    "Nihility": 9000,
+
+    # Эксклюзивы Лимбо
+
+    "Raven": 500000,
+
+    "Anima": 5500000,
+
+    "Juxtaposition": 40440400,
+
+    "Unknown": 444444444,
+
+    "Elude": 555555555,
+
+    "Prologue": 666616111,
+
+    "dreamscape": 850000000,
+
+    "NYCTOPHOBIA": 1011111010
+
+}
+
+
+
+GLOBAL_THRESHOLD = 9_999_999
+
+# Биомы и их настройки
+
+BIOMES = {
+
+    "Normal": {"chance": 0, "duration": 0, "multiplier": 1, "auras": []},
+
+    "Windy": {"chance": 1 / 500, "duration": 120, "multiplier": 3,
+
+              "auras": ["Wind", "Flow", "Stormal", "Stormal : Hurricane", "Aviator", "Maelstrom"]},
+
+    "Snowy": {"chance": 1 / 600, "duration": 120, "multiplier": 3,
+
+              "auras": ["Glacier", "Permafrost", "Blizzard", "Chillsear"]},
+
+    "Rainy": {"chance": 1 / 750, "duration": 120, "multiplier": 4,
+
+              "auras": ["Poseidon", "Sailor", "Sailor : Flying Dutchman", "Lumenpool", "Abyssal Hunter"]},
+
+    "Sand Storm": {"chance": 1 / 3000, "duration": 660, "multiplier": 4,
+
+                   "auras": ["Gilded", "Jackpot", "Anubis", "Atlas"]},
+
+    "Hell": {"chance": 1 / 6666, "duration": 660, "multiplier": 6,
+
+             "auras": ["Undead", "Undead : Devil", "Hades", "Felled", "Bloodlust"]},
+
+    "Starfall": {"chance": 1 / 7500, "duration": 600, "multiplier": 5,
+
+                 "auras": ["Starlight", "Star Rider", "Comet", "Astral", "Galaxy", "Stargazer", "Sirius",
+
+                           "Starscourge : Radiant", "Astral : Zodiac", "Gargantua"]},
+
+    "Corruption": {"chance": 1 / 9000, "duration": 660, "multiplier": 5,
+
+                   "auras": ["Hazard", "Corrosive", "Hazard : Rays", "Parasite", "Impeached"]},
+
+    "Null": {"chance": 1 / 10000, "duration": 99, "multiplier": 1000, "auras": ["Undefined", "Shift lock", "Nihility"]},
+
+    "Glitched": {"chance": 1 / 6000000, "duration": 184, "multiplier": 1,
+
+                 "auras": ["Oppression", "Glitch", "Fault", "⭐", "⭐⭐", "⭐⭐⭐", "Dreammetric"]},
+
+    "Dreamspace": {"chance": 1 / 5000000, "duration": 128, "multiplier": 1, "auras": ["⭐", "⭐⭐", "⭐⭐⭐", "Dreammetric"]}
+
+}
+
+# --- СЛОВАРЬ ДЛЯ GIF ---
+# Связывает название ауры с ID гифки.
+# Если вы не хотите гифку для какой-то ауры, просто удалите ее из этого словаря.
+# Логика ">= 1,000,000" все еще будет применяться.
+
+aura_gif_map = {
+
+    "Arcane": "https://t.me/solsrngbotcutscenes/3",
+
+    "Magnetic : Reverse Polarity": "https://t.me/solsrngbotcutscenes/6",
+
+    "Undefined": "https://t.me/solsrngbotcutscenes/7",
+
+    "Rage : Brawler": "https://t.me/solsrngbotcutscenes/11",
+
+    "Astral": "https://t.me/solsrngbotcutscenes/8",
+
+    "Cosmos": "https://t.me/solsrngbotcutscenes/9",
+
+    "Gravitational": "https://t.me/solsrngbotcutscenes/10",
+
+    "Bounded : Unbound": "https://t.me/solsrngbotcutscenes/12",
+
+    "Virtual": "https://t.me/solsrngbotcutscenes/87",
+
+    "Parasite": "https://t.me/solsrngbotcutscenes/13",
+
+    "Savior": "https://t.me/solsrngbotcutscenes/14",
+
+    "Shift lock": "https://t.me/solsrngbotcutscenes/49",
+
+    "Cosmos : Alice": "https://t.me/solsrngbotcutscenes/34",
+
+    "Aquatic : Flame": "https://t.me/solsrngbotcutscenes/66",
+
+    "Poseidon": "https://t.me/solsrngbotcutscenes/15",
+
+    "Zeus": "https://t.me/solsrngbotcutscenes/33",
+
+    "Solar : Solstice": "https://t.me/solsrngbotcutscenes/39",
+
+    "Galaxy": "https://t.me/solsrngbotcutscenes/35",
+
+    "Lunar : Full Moon": "https://t.me/solsrngbotcutscenes/40",
+
+    "Twilight": "https://t.me/solsrngbotcutscenes/36",
+
+    "Origin": "https://t.me/solsrngbotcutscenes/38",
+
+    "Hades": "https://t.me/solsrngbotcutscenes/88",
+
+    "Celestial : Divine": "https://t.me/solsrngbotcutscenes/37",
+
+    "Anubis": "https://t.me/solsrngbotcutscenes/41",
+
+    "Hyper-Volt": "https://t.me/solsrngbotcutscenes/42",
+
+    "Velocity": "https://t.me/solsrngbotcutscenes/43",
+
+    "Nautilus : Lost": "https://t.me/solsrngbotcutscenes/48",
+
+    "Harnessed": "https://t.me/solsrngbotcutscenes/44",
+
+    "Nihility": "https://t.me/solsrngbotcutscenes/45",
+
+    "Helios": "https://t.me/solsrngbotcutscenes/46",
+
+    "Stargazer": "https://t.me/solsrngbotcutscenes/47",
+
+    "Starscourge": "https://t.me/solsrngbotcutscenes/17",
+
+    "Sailor": "https://t.me/solsrngbotcutscenes/18",
+
+    "Stormal : Hurricane": "https://t.me/solsrngbotcutscenes/19",
+
+    "Sirius": "https://t.me/solsrngbotcutscenes/20",
+
+    "Arcane : Legacy": "https://t.me/solsrngbotcutscenes/67",
+
+    "Lullaby": "https://t.me/solsrngbotcutscenes/21",
+
+    "Chromatic": "https://t.me/solsrngbotcutscenes/22",
+
+    "Aviator": "https://t.me/solsrngbotcutscenes/23",
+
+    "Blizzard": "https://t.me/solsrngbotcutscenes/24",
+
+    "Arcane : Dark": "https://t.me/solsrngbotcutscenes/68",
+
+    "Ethereal": "https://t.me/solsrngbotcutscenes/25",
+
+    "Virtual : Fatal Error": "https://t.me/solsrngbotcutscenes/86",
+
+    "Overseer": "https://t.me/solsrngbotcutscenes/26",
+
+    "Exotic : Apex": "https://t.me/solsrngbotcutscenes/85",
+
+    "Matrix": "https://t.me/solsrngbotcutscenes/28",
+
+    "Runic": "https://t.me/solsrngbotcutscenes/27",
+
+    "Sentinel": "https://t.me/solsrngbotcutscenes/29",
+
+    "Twilight : Iridescent Memory": "https://t.me/solsrngbotcutscenes/30",
+
+    "Antivirus": "https://t.me/solsrngbotcutscenes/95",
+
+    "Carriage": "https://t.me/solsrngbotcutscenes/82",
+
+    "Sailor : Flying Dutchman": "https://t.me/solsrngbotcutscenes/31",
+
+    "Harnessed : Elements": "https://t.me/solsrngbotcutscenes/32",
+
+    "Virtual : WorldWide": "https://t.me/solsrngbotcutscenes/84",
+
+    "Chromatic : Genesis": "https://t.me/solsrngbotcutscenes/90",
+
+    "Starscourge : Radiant": "https://t.me/solsrngbotcutscenes/83",
+
+    "Overture": "https://t.me/solsrngbotcutscenes/50",
+
+    "Symphony": "https://t.me/solsrngbotcutscenes/52",
+
+    "Twilight : Withering Grace": "https://t.me/solsrngbotcutscenes/65",
+
+    "Felled": "https://t.me/solsrngbotcutscenes/96",
+
+    "Impeached": "https://t.me/solsrngbotcutscenes/81",
+
+    "Lumenpool": "https://t.me/solsrngbotcutscenes/97",
+
+    "Hyper-Volt : Ever-Storm": "https://t.me/solsrngbotcutscenes/80",
+
+    "Archangel": "https://t.me/solsrngbotcutscenes/53",
+
+    "Astral : Zodiac": "https://t.me/solsrngbotcutscenes/79",
+
+    "Exotic : Void": "https://t.me/solsrngbotcutscenes/75",
+
+    "Overture : History": "https://t.me/solsrngbotcutscenes/51",
+
+    "Bloodlust": "https://t.me/solsrngbotcutscenes/54",
+
+    "Maelstrom": "https://t.me/solsrngbotcutscenes/55",
+
+    "Lotusfall": "https://t.me/solsrngbotcutscenes/56",
+
+    "Jazz : Orchestra": "https://t.me/solsrngbotcutscenes/78",
+
+    "Atlas": "https://t.me/solsrngbotcutscenes/57",
+
+    "Flora : Evergreen": "https://t.me/solsrngbotcutscenes/16",
+
+    "Chillsear": "https://t.me/solsrngbotcutscenes/64",
+
+    "Abyssal Hunter": "https://t.me/solsrngbotcutscenes/58",
+
+    "Gargantua": "https://t.me/solsrngbotcutscenes/77",
+
+    "Apostolos": "https://t.me/solsrngbotcutscenes/59",
+
+    "Kyawthuite : Remembrance": "https://t.me/solsrngbotcutscenes/76",
+
+    "Ruins": "https://t.me/solsrngbotcutscenes/60",
+
+    "Matrix : Overdrive": "https://t.me/solsrngbotcutscenes/74",
+
+    "Sophyra": "https://t.me/solsrngbotcutscenes/61",
+
+    "Matrix : Reality": "https://t.me/solsrngbotcutscenes/73",
+
+    "Sovereign": "https://t.me/solsrngbotcutscenes/89",
+
+    "Ruins : Withered": "https://t.me/solsrngbotcutscenes/72",
+
+    "Aegis": "https://t.me/solsrngbotcutscenes/62",
+
+    "Pixelation": "https://t.me/solsrngbotcutscenes/63",
+
+    "Luminosity": "https://t.me/solsrngbotcutscenes/71",
+
+    "Equinox": "https://t.me/solsrngbotcutscenes/5",
+
+    "Glitch": "https://t.me/solsrngbotcutscenes/94",
+
+    "Oppression": "https://t.me/solsrngbotcutscenes/91",
+
+    "Dreammetric": "https://t.me/solsrngbotcutscenes/93",
+
+    # -------------- limbo gifs ----------------- #
+
+    "Anima": "https://t.me/solsrngbotcutscenes/104",
+
+    "Juxtaposition": "https://t.me/solsrngbotcutscenes/105",
+
+    "Unknown": "https://t.me/solsrngbotcutscenes/106",
+
+    "Elude": "https://t.me/solsrngbotcutscenes/107",
+
+    "Prologue": "https://t.me/solsrngbotcutscenes/108",
+
+    "dreamscape": "https://t.me/solsrngbotcutscenes/109",
+
+    "NYCTOPHOBIA": "https://t.me/solsrngbotcutscenes/110",
+
+}
+
+# все перчатки в игре
+
+gear_items = [
+
+    "[T1] Luck Glove", "[T1] Desire Glove", "[T1] Solar Device",
+
+    "[T2] Shining Star", "[T3] Exo Gauntlet", "[T3] Windstorm Device",
+
+    "[T4] Subzero Device", "[T5] Galactic Device", "[T5] Volcanic Device",
+
+    "[T6] Exoflex Device", "[T6] Hologrammer", "[T7] Ragnaröker",
+
+    "[T8] Starshaper", "[T9] Neurolyzer", "[T10] Genesis Drive"
+
+]
+
+# Все баффы перчаток
+
+luck_bonuses = {
+
+    "[T1] Luck Glove": 0.8,
+
+    "[T1] Desire Glove": 1.4,
+
+    "[T1] Solar Device": 2.3,
+
+    "[T2] Shining Star": 2,  # Базовый (че ты имеешь ввиду под этим)
+
+    "[T3] Exo Gauntlet": 3.5,
+
+    "[T3] Windstorm Device": 4.5,
+
+    "[T4] Subzero Device": 5,
+
+    "[T5] Galactic Device": 6.8,
+
+    "[T5] Volcanic Device": 7.25,
+
+    "[T6] Exoflex Device": 9.2,
+
+    "[T6] Hologrammer": 10,
+
+    "[T7] Ragnaröker": 13.5,  # Базовый (???)
+
+    "[T8] Starshaper": 27.5,
+
+    "[T9] Neurolyzer": 45.25,
+
+    "[T10] Genesis Drive": 65
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+**Sols rng Telegram bot**
+
+Sols rng Telegram bot Official dev repository.
+
+To-do:
+Fix AutoRoll
+Make Luck like in sols rng
+Add every aura from the sols rng
